97b35f94c8bd5580924b19173ffcb76d6f7359d8
Sergey Cherkashin <cherkashin@vuflya.ru>
2022-04-05 00:03:52 +0300

    feat: add mongo db

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT
-const HOST = process.env.HOST
+const { connectDb } = require('./helpers/db')
+const { host, port, db } = require('./configuration')
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started api service on port: ${port}`)
+    console.log(`On host: ${host}`)
+    console.log(`Our database: ${db}`)
+  })
+}
 
 app.get('/test', (req, res) => {
   res.send('Our api server is working correctly!')
 })
 
-app.listen(PORT, () => {
-  console.log(`Started api service on port: ${PORT}`)
-  console.log(`On host: ${HOST}`)
-})
-
-connectDb.on('error', console.log).on('disconnect')
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer)
